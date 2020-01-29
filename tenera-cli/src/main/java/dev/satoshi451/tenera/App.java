22fd6279718c0c75751e6b677b4f08e271ca683b
wiseman <votrin.and@yandex.ru>
2020-01-30 01:54:58 +0700

    Big Bang!

---
+++ tenera-cli/src/main/java/dev/satoshi451/tenera/App.java
@@ -0,0 +1,13 @@
+package dev.satoshi451.tenera;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tenera-cli/src/test/java/dev/satoshi451/tenera/AppTest.java
@@ -0,0 +1,20 @@
+package dev.satoshi451.tenera;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
